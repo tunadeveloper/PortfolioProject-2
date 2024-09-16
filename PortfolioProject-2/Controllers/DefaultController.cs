310cc7663ca1424b40ea65cec4954a459a0e9a8a
tunadeveloper <tunahancengizbusiness@gmail.com>
2024-09-16 07:50:00 +0300

    Add project files.

---
+++ PortfolioProject-2/Controllers/DefaultController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PortfolioProject_2.Models.Entity;
+
+namespace PortfolioProject_2.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        MyPortfolioDb2Entities4 db = new MyPortfolioDb2Entities4();
+        public ActionResult Index()
+        {
+            var values = db.About.ToList();
+            return View(values);
+        }
+
+        public PartialViewResult Experience()
+        {
+            var experinces= db.Experience.ToList();
+            return PartialView(experinces);
+        }
+
+        public PartialViewResult Education()
+        {
+            var educations = db.Education.ToList();
+            return PartialView(educations);
+        }
+
+       public PartialViewResult Skill()
+        {
+            var skills = db.Skill.ToList();
+            return PartialView(skills);
+        }
+
+        public PartialViewResult Interest() {
+            
+            var inretests = db.Interests.ToList();
+            return PartialView(inretests);
+        }
+
+        public PartialViewResult Certificate()
+        {
+            var certificates = db.Certificates.ToList();
+            return PartialView(certificates);
+        }
+
+        public PartialViewResult Contact() {
+            
+
+            return PartialView();
+        }
+    }
+}
