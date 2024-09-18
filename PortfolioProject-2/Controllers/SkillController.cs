f29483e51067006e5e87de912a0ba0f0429cf823
tunadeveloper <tunahancengizbusiness@gmail.com>
2024-09-18 21:35:17 +0300

    Update

--- PortfolioProject-2/Controllers/SkillController.cs
+++ PortfolioProject-2/Controllers/SkillController.cs
@@ -19,5 +19,41 @@ namespace PortfolioProject_2.Controllers
             var skill = repo.List();
             return View(skill);
         }
+
+        [HttpGet]
+        public ActionResult AddSkill()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddSkill(Skill skill)
+        {
+            repo.TAdd(skill);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteSkill(int id)
+        {
+           var skill = repo.Find(x => x.Id == id);
+            repo.TRemove(skill);
+           return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult UpgradeSkill(int id)
+        {
+            var skill = repo.Find(x => x.Id == id);
+            return View(skill);
+        }
+        [HttpPost]
+        public ActionResult UpgradeSkill(Skill skill)
+        {
+            var s = repo.Find(x => x.Id == skill.Id);
+            s.Skills = skill.Skills;
+            s.Value = skill.Value;
+            repo.TUpdate(skill);
+            return RedirectToAction("Index");
+        }
     }
 }
